420b20aed028eafe1e3fc01b788956013d0edbf5
Aman <ratheeaman65@gmail.com>
2024-10-13 16:18:44 +0530

    copy-button created

---
+++ src/components/copy-button.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react"
+import { cn } from "@/lib/utils"
+import Button from "@/registry/bnbr/ui/button"
+import { CheckIcon, ClipboardIcon } from "lucide-react"
+
+
+
+export interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    asChild?: boolean
+}
+
+
+
+interface CopyButtonProps extends ButtonProps {
+    value: string
+}
+
+
+export async function copyToClipboardWithMeta(value: string) {
+    navigator.clipboard.writeText(value)
+}
+
+
+export function CopyButton({ value, className, ...props }: CopyButtonProps) {
+    const [hasCopied, setHasCopied] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setHasCopied(false)
+        }, 2000)
+    }, [hasCopied])
+
+    return (
+        <Button
+            className={cn("relative z-10 px-1 py-1 text-zinc-400 hover:bg-zinc-700 hover:text-zinc-300 bg-transparent", className)}
+            onClick={() => {
+                copyToClipboardWithMeta(value)
+                setHasCopied(true)
+            }}
+            {...props}
+        >
+            <span className="sr-only">Copy</span>
+            {hasCopied ? <CheckIcon height={'20px'} width={'20px'} /> : <ClipboardIcon height={'20px'} width={'20px'} />}
+        </Button>
+    )
+}
