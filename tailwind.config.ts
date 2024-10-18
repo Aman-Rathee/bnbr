7de8da79c7d6ee1eda25d5c3d73ea4309a16c9d6
Aman <ratheeaman65@gmail.com>
2024-10-18 20:45:33 +0530

    css for button animated border component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,42 @@ const config: Config = {
     "./src/scripts/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      keyframes: {
+        'border-spin': {
+          '0%': {
+            left: '0%',
+            top: '0%',
+            transform: 'translate(-50%, -50%)'
+          },
+          '38%': {
+            left: '100%',
+            top: '0%',
+            transform: 'translate(-50%, -50%)'
+          },
+          '50%': {
+            left: '100%',
+            top: '100%',
+            transform: 'translate(-50%, -50%)'
+          },
+          '88%': {
+            left: '0%',
+            top: '100%',
+            transform: 'translate(-50%, -50%)'
+          },
+          '100%': {
+            left: '0%',
+            top: '0%',
+            transform: 'translate(-50%, -50%)'
+          }
+        },
+      },
+      animation: {
+        'border-spin': 'border-spin 5s linear infinite'
+      }
+    }
+  },
   plugins: [],
 };
 export default config;
