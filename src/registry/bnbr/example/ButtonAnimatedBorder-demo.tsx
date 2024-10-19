41d7bdb42d1fbbe093c120c40ed34df25624e62e
Aman <ratheeaman65@gmail.com>
2024-10-19 14:38:23 +0530

    created demo component for ButtonAnimatedBorder

---
+++ src/registry/bnbr/example/ButtonAnimatedBorder-demo.tsx
@@ -0,0 +1,11 @@
+import AnimatedBorder from '@/registry/bnbr/ui/ButtonAnimatedBorder'
+
+function ButtonAnimatedBorder() {
+    return (
+        <AnimatedBorder className=''>
+            Bnbr Ui
+        </AnimatedBorder>
+    )
+}
+
+export default ButtonAnimatedBorder
