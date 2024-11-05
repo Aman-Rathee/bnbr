1304e72f11cbc8bfb048c0c1b32989299b0b64a7
Aman <ratheeaman65@gmail.com>
2024-11-05 22:52:02 +0530

    added demo component for marquee animation

---
+++ src/registry/bnbr/example/marquee-animation-demo.tsx
@@ -0,0 +1,44 @@
+import { Marquee } from "@/registry/bnbr/ui/marquee-animation";
+
+const cardsData = [
+    {
+        heading: "Responsive",
+        paragraph: "Learn how to create responsive layouts that adapt seamlessly to various devices and screen sizes."
+    },
+    {
+        heading: "Animations",
+        paragraph: "Discover techniques to implement smooth animations and transitions to enhance user interactions."
+    },
+    {
+        heading: "Accessibility",
+        paragraph: "Explore the best practices to ensure your web applications are accessible to all users and devices."
+    },
+    {
+        heading: "State",
+        paragraph: "Master state management patterns that simplify data flow and ensure a maintainable codebase."
+    },
+    {
+        heading: "Styling",
+        paragraph: "Understand modern approaches to component styling for scalable, consistent design systems."
+    },
+    {
+        heading: "Server-Side",
+        paragraph: "Learn the benefits of server-side rendering and how it improves performance and SEO."
+    },
+    {
+        heading: "API",
+        paragraph: "Explore techniques for integrating APIs seamlessly into your web applications with ease."
+    },
+    {
+        heading: "Testing",
+        paragraph: "Understand different testing strategies to improve code quality and user experience consistently."
+    }
+];
+
+export default function Home() {
+    return (
+        <>
+            <Marquee className="" cardsData={cardsData} />
+        </>
+    );
+}
