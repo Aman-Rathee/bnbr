2211cdb61757e881043a1f474f4c56f467f5b25e
Aman <ratheeaman65@gmail.com>
2024-11-21 21:20:14 +0530

    setup the default and dark theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,34 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        border: 'rgb(var(--border-color))',
+        // border: 'rgb(var(--border-color))',
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       keyframes: {
         'border-spin': {
