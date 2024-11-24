3e4d4184dc176a13ea562ce21a999ba5a3174fa6
Aman <ratheeaman65@gmail.com>
2024-11-24 21:05:33 +0530

    made bg-gradient component light and dark theme supported

--- src/registry/bnbr/example/bg-gradient-demo.tsx
+++ src/registry/bnbr/example/bg-gradient-demo.tsx
@@ -5,7 +5,7 @@ function BgGradientDemo() {
   return (
     <BgGradient className=''>
       <div className="space-y-4">
-        <div className="text-2xl">
+        <div className="text-2xl text-black dark:text-white">
           Gradient Card
         </div>
         <Image
@@ -14,7 +14,7 @@ function BgGradientDemo() {
           height={500}
           alt="Picture of the author"
         />
-        <p className="text-sm text-muted-foreground">
+        <p className="text-sm text-gray-600 dark:text-gray-300">
           This is a simple card component with a gradient background. You can easily change the colors and content to fit your needs. Use it to highlight important information or create eye-catching sections in your UI.
         </p>
       </div>
